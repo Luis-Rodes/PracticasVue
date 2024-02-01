b7f5181613c4a9d323a12d8133d7c5eeea09c13d
Luis Rodes <128967322+Luis-Rodes@users.noreply.github.com>
2024-02-01 19:51:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      Nombre: 'Luis',
+      Apellidos : 'Rodes Roca',
+      foto : 'https://tse3.mm.bing.net/th?id=OIP.LVPLwc2LP3K0LDPLPw7B7gC0C0&pid=Api',
+      Edad : 52
+    }
+  })
